6cc684f1a123f2d065092c6aafb0c7cc819e6cf4
Hyper-TH <twila.shayne.habab@outlook.com>
2023-10-14 18:25:38 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Person } from './components/Person';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+		<Person
+			name="Hyper"
+			email="hyper@max.com"
+			age={22}
+			isMarried={true}
+			friends={["GGanon", "Glimpse", "Cheedo", "Ely", "TopKid", "NightSky"]}
+		/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,13 @@
+export const Person = (props) => {
+    return (
+        <div>
+            <h1>Name: {props.name}</h1>
+            <h1>Email: {props.email}</h1>
+            <h1>Age: {props.age}</h1>
+            <h1>This person {props.isMarried ? "is" : "is not"} married </h1>
+            {props.friends.map((friend) => (
+                <h2> {friend} </h2>
+            ))}
+        </div>
+    );
+}
